222fe72b315197831e0cbf3ffae62df22a6f4bd1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2017-02-13 22:38:33 +0100

    Update Kotlin to 1.0.6 and Wicket to 8.0.0-M4

--- src/main/kotlin/com/mycompany/HomePage.kt
+++ src/main/kotlin/com/mycompany/HomePage.kt
@@ -4,10 +4,10 @@ import org.apache.wicket.request.mapper.parameter.PageParameters
 import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.markup.html.WebPage
 
-class HomePage : WebPage {
-	private val serialVersionUID: Long = 1L
+class HomePage(parameters: PageParameters) : WebPage(parameters) {
 
-	constructor(parameters: PageParameters) : super(parameters) {
+	init {
 		add(Label("version", application.frameworkSettings.version))
 	}
+
 }
